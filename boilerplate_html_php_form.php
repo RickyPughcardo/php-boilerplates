6cb042e1bc30c063bf503da338ce3d98292801d9
Ricky Pugh <ricky@getpantheon.com>
2014-12-31 20:28:03 -0600

    Pushing self-referential form boilerplate

---
+++ boilerplate_html_php_form.php
@@ -0,0 +1,18 @@
+<html>
+	<head>
+		<title>Boilerplate Form</title>
+	</head>
+	<body>
+		<form action="" method="post">
+ 			 Enter some text, cowboy:
+  			<input name="text" type="text" maxlength="100" />
+ 			<input type="submit" value="Check it!">
+		</form>
+		<?php
+		//self-referential 
+  		if (isset($_POST["text"]) && !empty($_POST["text"]))
+  		echo "Voila! Like magic, you've submitted: ".$_POST["text"].".";
+  		else echo " ";   
+		?>
+	</body>
+</html>
